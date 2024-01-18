23c5b795a04be1ad37971772f0a8f79c1590a557
oruchankural <oruchankural@gmail.com>
2024-01-18 11:58:55 +0300

    Initial Commit.

---
+++ generic_pipeline_builder/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace generic_pipeline_builder.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
