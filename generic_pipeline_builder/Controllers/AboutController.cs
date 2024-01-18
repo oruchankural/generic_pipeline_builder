384abf49799b703aa7ef7934e8d5acf80d4b728e
oruchankural <oruchankural@gmail.com>
2024-01-18 13:04:51 +0300

    About Page Added.

---
+++ generic_pipeline_builder/Controllers/AboutController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace generic_pipeline_builder.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly ILogger<AboutController> _logger;
+
+        public AboutController(ILogger<AboutController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
